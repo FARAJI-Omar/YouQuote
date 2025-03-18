8007d88d0f2ed680aed78d8f231219be2f7eafab
F99 <farajiomar99@gmail.com>
2025-03-18 14:19:18 +0000

    Quote API: refactor authentication routes to use middleware

--- youquote/routes/api.php
+++ youquote/routes/api.php
@@ -6,22 +6,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::post('/auth/register', [UserController::class, 'register']);
+Route::post('/auth/login', [UserController::class, 'login']);
+
 
 Route::get('/quotes/random', [QuoteController::class, 'random']);
 Route::get('/quotes/filter', [QuoteController::class, 'filter']);
 Route::get('/quotes/popular', [QuoteController::class, 'popular']);
 
-Route::post('/auth/register', [UserController::class, 'register']);
-Route::post('/auth/login', [UserController::class, 'login']);
-Route::post('/auth/logout', [UserController::class, 'logout']);
-
 
 Route::apiResource('quotes', QuoteController::class);
 
 
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/auth/logout', [UserController::class, 'logout']);
+});
 
-
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
